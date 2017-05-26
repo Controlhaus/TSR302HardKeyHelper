dd02e6170cf3ee59082a118a6a3bdd950ce3e507
Patrick Murray <patrick@controlhaus.de>
2017-05-26 09:03:18 +0200

    Update TSR302HardKeyHelper.cs
    Changed class to static.
    Changed dictionary to enum.
    Added IsDefined methods.

--- TSR302HardKeyHelper.cs
+++ TSR302HardKeyHelper.cs
@@ -6,52 +6,62 @@ using Crestron.SimplSharp;
 
 namespace MyNamespace
 {
-    public class TSR302HardKeyHelper
+    public static class TSR302HardKeyHelper
     {
-        public Dictionary<uint, string> hardKeys = new Dictionary<uint, string>()
+        public enum HardKeys
         {
-            { 1, "power" },
-            { 2, "home" },
-            { 3, "menu" },
-            { 4, "guide" },
-            { 5, "info" },
-            { 6, "volumeUp" },
-            { 7, "volumeDown" },
-            { 13, "channelUp" },
-            { 14, "channelDown" },
-            { 15, "mute" },
-            { 16, "exit" },
-            { 17, "last" },
-            { 8, "up" },
-            { 9, "down" },
-            { 10, "left" },
-            { 11, "right" },
-            { 12, "select" },
-            { 18, "play" },
-            { 19, "pause" },
-            { 20, "scanReverse" },
-            { 21, "scanForward" },
-            { 22, "skipReverse" },
-            { 23, "skipForward" },
-            { 24, "stop" },
-            { 26, "dvr" },
-            { 25, "record" },
-            { 27, "numberPad1" },
-            { 28, "numberPad2" },
-            { 29, "numberPad3" },
-            { 30, "numberPad4" },
-            { 31, "numberPad5" },
-            { 32, "numberPad6" },
-            { 33, "numberPad7" },
-            { 34, "numberPad8" },
-            { 35, "numberPad9" },
-            { 36, "numberPad0" },
-            { 37, "clear" },
-            { 38, "enter" },
-            { 39, "red" },
-            { 40, "green" },
-            { 41, "yellow" },
-            { 42, "blue" }
+             power = 1,
+             home = 2,
+             menu = 3,
+             guide =4 ,
+             info = 5,
+             volumeUp = 6,
+             volumeDown = 7,
+             channelUp = 13,
+             channelDown = 14,
+             mute = 15,
+             exit = 16,
+             last = 17,
+             up = 8,
+             down = 9,
+             left = 10,
+             right = 11,
+             select = 12,
+             play = 18,
+             pause = 19,
+             scanReverse = 20,
+             scanForward = 21,
+             skipReverse = 22,
+             skipForward = 23,
+             stop = 24,
+             dvr = 26,
+             record = 25,
+             numberPad1 = 27,
+             numberPad2 = 28,
+             numberPad3 = 29,
+             numberPad4 = 30,
+             numberPad5 = 31,
+             numberPad6 = 32,
+             numberPad7 = 33,
+             numberPad8 = 34,
+             numberPad9 = 35,
+             numberPad0 = 36,
+             clear = 37,
+             enter = 38,
+             red = 39,
+             green = 40,
+             yellow = 41,
+             blue = 42
         };
+
+        public static bool IsDefined(string name)
+        {
+            return Enum.IsDefined(typeof(HardKeys), name);
+        }
+
+        public static bool IsDefined(HardKeys value)
+        {
+            return Enum.IsDefined(typeof(HardKeys), value);
+        }        
     }
 }
